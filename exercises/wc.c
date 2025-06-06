72d1c55c22c10fc21f8ff8f3a90e3ca81ee7a6a1
DannyBimma <dantrotman92@gmail.com>
2025-06-06 10:14:54 -0400

    Modify console output to reflect changes.

--- exercises/wc.c
+++ exercises/wc.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    // Count chars, words, sentences, and lines from stream
-    int chars, words, sentences, lines = 0;
+    // Count: chars, words, statements, and lines
+    int chars, words, statements, lines = 0;
 
     for (int c_lit = getchar(); c_lit != EOF; ++chars) {
         if (c_lit == ' ')
             ++words;
 
         if (c_lit == '.' || c_lit == '?' || c_lit == '!')
-            ++sentences;
+            ++statements;
 
         if (c_lit == '\n')
             ++lines;
     }
 
-    printf("Text stream contains:\n%d characters\n %d words\n %d lines\n\n", chars, words, lines);
+    printf("Text stream contains:\n%d characters\n %d words\n %d statements\n %d lines\n\n", chars, words, statements, lines);
+
+    return 0;
 }
