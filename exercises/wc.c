ee5f4a601b2d8b543de31f01d5396a4391c1ed47
DannyBimma <dantrotman92@gmail.com>
2025-06-06 10:07:55 -0400

    Swap out the while loop for a for loop.

--- exercises/wc.c
+++ exercises/wc.c
@@ -4,19 +4,16 @@ int main(void) {
     // Count chars, words, sentences, and lines from stream
     int chars, words, sentences, lines = 0;
 
-    while ((c = getchar()) != EOF) {
-        ++chars;
-
-        if (c == '\n')
-            ++lines;
-
-        if (c == ' ' || c == '\t' || c == '\n')
-            state = OUT;
-        else if (state == OUT) {
-            state = IN;
+    for (int c_lit = getchar(); c_lit != EOF; ++chars) {
+        if (c_lit == ' ')
             ++words;
-        }
 
+        if (c_lit == '.' || c_lit == '?' || c_lit == '!')
+            ++sentences;
+
+        if (c_lit == '\n')
+            ++lines;
     }
+
     printf("Text stream contains:\n%d characters\n %d words\n %d lines\n\n", chars, words, lines);
 }
