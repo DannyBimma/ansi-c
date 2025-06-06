c17a2b382ea5a0a473fc887d65674e4e8e22a122
DannyBimma <dantrotman92@gmail.com>
2025-06-05 22:55:15 -0400

    I'm tireds

---
+++ exercises/wc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main(void) {
+    // Count chars, words, and lines from stream
+    int c;
+    int state;
+    int lines, words, chars = 0;
+
+    while ((c = getchar()) != EOF) {
+        ++chars;
+
+        if (c == '\n')
+            ++lines;
+
+        if (c == ' ' || c == '\t' || c == '\n')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++words;
+        }
+
+    }
+    printf("Text stream contains:\n%d characters\n %d words\n %d lines\n\n", chars, words, lines);
+}
