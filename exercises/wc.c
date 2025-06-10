1ee95dc6176b3a301c13666b4bff87971d1445db
DannyBimma <dantrotman92@gmail.com>
2025-06-10 19:41:36 -0400

    Rearrange control flow statements.

--- exercises/wc.c
+++ exercises/wc.c
@@ -6,23 +6,22 @@
 int main(void) {
     // Count: chars, words, statements, and lines
     int chars = 0, words = 0, statements = 0, lines = 0;
-    int state = IN;
+    int state;
 
     for (int c_lit = getchar(); c_lit != EOF; c_lit = getchar()) {
         ++chars;
+
+        if (c_lit == '\n')
+            ++lines;
+
+        if (c_lit == '.' || c_lit == '?' || c_lit == '!')
+            ++statements;
 
         if (c_lit == ' ' || c_lit == '\n' || c_lit == '\t')
             state = OUT;
         else if (state == OUT) {
             state = IN;
             ++words;
-        }
-
-        if (c_lit == '.' || c_lit == '?' || c_lit == '!')
-            ++statements;
-
-        if (c_lit == '\n') {
-            ++lines;
         }
     }
     printf("Text stream contains:\n%d characters\n %d words\n %d statements\n %d lines\n\n", chars, words, statements, lines);
