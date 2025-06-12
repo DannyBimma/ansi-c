00260486ee2e93d843bc8cd07604c22bd6f57e66
DannyBimma <dantrotman92@gmail.com>
2025-06-12 19:41:27 -0400

    Actually assign char to print.

--- exercises/wp.c
+++ exercises/wp.c
@@ -5,15 +5,13 @@
 
 
 int main(void) {
-
     int chars = 0;
     int state = OUT;
 
     for (int c_lit = getchar(); c_lit != EOF; c_lit = getchar(), ++chars) {
 
         if (c_lit != ' ' && c_lit != '\n' && c_lit != '\t') {
-
-            char c;
+            char c = c_lit;
             printf("%c", c);
             printf("\n");
         }
@@ -23,6 +21,4 @@ int main(void) {
         else if (state == OUT)
             state = IN;
     }
-
-    return 0;
 }
