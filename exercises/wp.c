6c0cab1c03448c785f69e779431f927337685b28
DannyBimma <dantrotman92@gmail.com>
2025-06-12 19:35:44 -0400

    Build word printing program.

--- exercises/wp.c
+++ exercises/wp.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
 
-int main(void) {}
+#define IN 1
+#define OUT 0
+
+
+int main(void) {
+
+    int chars = 0;
+    int state = OUT;
+
+    for (int c_lit = getchar(); c_lit != EOF; c_lit = getchar(), ++chars) {
+
+        if (c_lit != ' ' && c_lit != '\n' && c_lit != '\t') {
+
+            char c;
+            printf("%c", c);
+            printf("\n");
+        }
+
+        if (c_lit == ' ' || c_lit == '\n' || c_lit == '\t')
+            state = OUT;
+        else if (state == OUT)
+            state = IN;
+    }
+
+    return 0;
+}
