586f059da99ff9eec3c3693a17d392c53af21327
DannyBimma <dantrotman92@gmail.com>
2025-06-18 21:38:49 -0400

    Implement main program logic.

--- exercises/array_counter.c
+++ exercises/array_counter.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 
-int main() {}
+int main(void) {
+    int n_white = 0, n_other = 0;
+    int n_digit[10];
+    int c;
+
+    for (int i = 0; i < 10; i++)
+        n_digit[i] = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c >= 0 && c <= 9)
+            ++n_digit[c-'0'];
+        else if (c == '' || c == '\n' || c == '\t')
+            ++n_white;
+        else
+            ++n_other;
+    }
+}
