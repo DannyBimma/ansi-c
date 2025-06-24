ef4d3cfe6614f4f152126765aa165c374151572b
DannyBimma <dantrotman92@gmail.com>
2025-06-23 21:47:57 -0400

    Define func to get lines.

--- exercises/longest-line/ll.c
+++ exercises/longest-line/ll.c
@@ -7,3 +7,19 @@ int main(void) {
     char current_line[MAXLINE];
     char longest_line[MAXLINE];
 }
+
+int get_line(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
