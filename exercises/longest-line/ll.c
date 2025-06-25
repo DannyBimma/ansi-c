83f6a1a1654aa3585064928ae08b984985b4a87c
DannyBimma <dantrotman92@gmail.com>
2025-06-24 21:34:45 -0400

    Reformat back to K&R.

--- exercises/longest-line/ll.c
+++ exercises/longest-line/ll.c
@@ -4,48 +4,42 @@
 int get_line(char s[], int lim);
 void copy(char to[], char from[]);
 
-int main(void)
-{
-    int current_line_len;
-    int current_max = 0;
-    char current_line[MAXLINE];
-    char longest_line[MAXLINE];
+int main(void) {
+  int current_line_len;
+  int current_max = 0;
+  char current_line[MAXLINE];
+  char longest_line[MAXLINE];
 
-    while ((current_line_len = get_line(current_line, MAXLINE)) > 0)
-    {
-        if (current_line_len > current_max)
-        {
-            current_max = current_line_len;
-            copy(longest_line, current_line);
-        }
+  while ((current_line_len = get_line(current_line, MAXLINE)) > 0) {
+    if (current_line_len > current_max) {
+      current_max = current_line_len;
+      copy(longest_line, current_line);
     }
-    if (current_max > 0)
-        printf("%s\n", longest_line);
+  }
+  if (current_max > 0)
+    printf("%s\n", longest_line);
 
-    return 0;
+  return 0;
 }
 
-int get_line(char s[], int lim)
-{
-    int c, i;
+int get_line(char s[], int lim) {
+  int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-        s[i] = c;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    s[i] = c;
 
-    if (c == '\n')
-    {
-        s[i] = c;
-        ++i;
-    }
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
 
-    s[i] = '\0';
-    return i;
+  s[i] = '\0';
+  return i;
 }
 
-void copy(char to[], char from[])
-{
-    int i = 0;
+void copy(char to[], char from[]) {
+  int i = 0;
 
-    while ((to[i] = from[i]) != '\0')
-        ++i;
+  while ((to[i] = from[i]) != '\0')
+    ++i;
 }
