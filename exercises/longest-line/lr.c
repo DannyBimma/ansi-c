0adc865100862778736512e27367ed42ce694c05
DannyBimma <dantrotman92@gmail.com>
2025-06-27 10:04:55 -0400

    Fix name clash with system func, and compile.

--- exercises/longest-line/lr.c
+++ exercises/longest-line/lr.c
@@ -4,17 +4,19 @@
 #define MAXLINE 1000
 
 void reverse(char s[]);
-int getline(char line[], int maxline);
+int get_line(char line[], int maxline);
 
 // White Book C: Exercise 1-19
 int main() {
   char line[MAXLINE];
 
-  while (getline(line, MAXLINE) > 0) {
+  while (get_line(line, MAXLINE) > 0) {
     reverse(line);
 
     printf("%s", line);
   }
+
+  printf("\n");
 
   return 0;
 }
@@ -43,7 +45,7 @@ void reverse(char s[]) {
   }
 }
 
-int getline(char s[], int lim) {
+int get_line(char s[], int lim) {
   int c, i;
 
   for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
