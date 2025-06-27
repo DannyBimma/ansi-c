d9d0ee92027bb7cada13fb56b0890198cc6dcfbd
DannyBimma <dantrotman92@gmail.com>
2025-06-27 18:56:44 -0400

    Start main and write helpers.

---
+++ exercises/longest-line/ll_extern.c
@@ -0,0 +1,45 @@
+/* This is the longest_line program re-written
+ * using external variables and scope */
+
+#include <stdio.h>
+#include <sys/resource.h>
+
+#define MAXLINE 1000
+
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+int main(void) {
+    int len;
+    extern int max;
+    extern char longest[];
+
+
+}
+
+int getline(void) {
+    int c, i;
+    extern char line[];
+
+    for (i = 0; i < MAXLINE-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+
+    if (c == '\n') {
+        line[i] = '\0';
+        ++i;
+    }
+
+    line[i] = '\0';
+    return i;
+}
+
+void copy(void) {
+    int i;
+    extern char line[], longest[];
+
+    i = 0;
+
+    while ((longest[i] = line[i]) != '\0')
+        ++i;
+}
