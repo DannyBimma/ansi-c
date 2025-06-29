1fe673b30e66bba53c30f2c06669422678f80fb5
DannyBimma <dantrotman92@gmail.com>
2025-06-28 20:48:49 -0400

    Write lexical analysis program.

---
+++ exercises/lexical-analysis/test_balanced.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    if (x > 0) {
+        printf("Positive");
+    } else {
+        printf("Not positive");
+    }
+    return 0;
+}
---
+++ exercises/lexical-analysis/test_unbalanced.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10;
+    if (x > 0 {
+        printf("Missing closing paren");
+    } else {
+        printf("Missing bracket");
+    
+    return 0;
