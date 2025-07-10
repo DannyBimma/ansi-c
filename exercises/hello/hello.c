0c4e0605710fde21b6a609ba55fe3bf889a5bab1
DannyBimma <dantrotman92@gmail.com>
2025-07-10 18:48:08 -0400

    Add copyright msg as per linter.

--- exercises/hello/hello.c
+++ exercises/hello/hello.c
@@ -1,6 +1,9 @@
+/* Copyright 2025 DannyBimma */
+
 #include <stdio.h>
 
-int main(void)
-{
-    printf("H\te\tl\tl\to,\n \nw\to\tr\tl\td!\n");
+int main(void) {
+  printf("H\te\tl\tl\to,\n \nw\to\tr\tl\td!\n");
+
+  return 0;
 }
