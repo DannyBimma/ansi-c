154ad2201dac4dff9eb1071b6c0d0db9f0db377e
DannyBimma <dantrotman92@gmail.com>
2025-07-21 20:47:49 -0400

    Right-justify the numbers in their fields.

--- exercises/celsius-to-fahrenheit/f_to_c.c
+++ exercises/celsius-to-fahrenheit/f_to_c.c
@@ -16,7 +16,7 @@ int main(void) {
   while (fahr <= upper) {
     celcius = 5 * (fahr - 32) / 9;
 
-    printf("%d\t%d\n", fahr, celcius);
+    printf("%3d\t%6d\n", fahr, celcius);
 
     fahr = fahr + step;
   }
