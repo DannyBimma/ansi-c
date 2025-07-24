fd338fa772669821ea7b7c5679d9fdaf9d7fb699
DannyBimma <dantrotman92@gmail.com>
2025-07-24 10:50:13 -0400

    Complete and compile Ex: 1-5.

--- exercises/temp-table/f2c_re.c
+++ exercises/temp-table/f2c_re.c
@@ -5,3 +5,19 @@
  *Exercise 1-5: Print the temp conversion in
  reverse order. ✅
  */
+
+#include <stdio.h>
+
+int main(void) {
+  int fahr;
+
+  // Output heading
+  printf("FAHRENHEIT TO CELSIUS TABLE\n");
+  printf("\n");
+
+  for (fahr = 300; fahr >= 0; fahr -= 20) {
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  }
+
+  return 0;
+}
