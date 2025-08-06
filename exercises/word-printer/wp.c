b6aa8db254f58f137df8e0f3efa458b616d84534
DannyBimma <dantrotman92@gmail.com>
2025-08-05 22:13:36 -0400

    Bleh.

--- exercises/word-printer/wp.c
+++ exercises/word-printer/wp.c
@@ -1,29 +1,27 @@
+/*Copyright (c) 2025 DannyBimma. All Rights Reserved.*/
 #include <stdio.h>
 
 #define IN 1
 #define OUT 0
 
-
 int main(void) {
-    int chars = 0;
-    int state = OUT;
-
-    // Print each word in file on a new line
-    for (int c_lit = getchar(); c_lit != EOF; c_lit = getchar(), ++chars) {
-
-        if (c_lit != ' ' && c_lit != '\n' && c_lit != '\t') {
-            char c = c_lit;
-            printf("%c", c);
-        }
-
-        if (c_lit == ' ' || c_lit == '\n' || c_lit == '\t') {
-            state = OUT;
-            printf("\n");
-        }
-        else if (state == OUT)
-            state = IN;
+  int chars = 0;
+  int state = OUT;
+  // Print each word in file on a new line
+  for (int c_lit = getchar(); c_lit != EOF; c_lit = getchar(), ++chars) {
+    if (c_lit != ' ' && c_lit != '\n' && c_lit != '\t') {
+      char c = c_lit;
+      printf("%c", c);
     }
 
-    printf("\n");
-    return 0;
+    if (c_lit == ' ' || c_lit == '\n' || c_lit == '\t') {
+      state = OUT;
+      printf("\n");
+    } else if (state == OUT)
+      state = IN;
+  }
+
+  printf("\n");
+
+  return 0;
 }
