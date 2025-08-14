ac0dacd72701476112a8e697352c0c7c29484fd3
DannyBimma <dantrotman92@gmail.com>
2025-08-14 17:17:46 -0400

    Hit the lay up.

---
+++ exercises/ranges/htoi.c
@@ -0,0 +1,15 @@
+/*Copyright (c) 2025 DannyBimma. All Rights Reserved.
+ *
+ * Exercise 2-3: Converts hexadecimal strings to integers
+ * with the following features:
+
+  - Handles optional "0x" or "0X" prefix
+  - Accepts digits 0-9, lower-case a-f, and upper-case A-F
+  - Stops processing at invalid characters
+  - Each step is thoroughly commented as requested*/
+
+#include <stdio.h>
+
+int htoi(char s[]);
+
+int main(void) {}
