95abd0a49010fc731aa733162c5974acb98070ea
DannyBimma <dantrotman92@gmail.com>
2025-08-19 16:28:25 -0400

    Helper func.

--- exercises/operations/squeeze_deeznuts.c
+++ exercises/operations/squeeze_deeznuts.c
@@ -3,3 +3,15 @@
  * An alternate version of squeeze(s1,s2) that deletes
  * each character in s1 that matches any character in
  * the string s2*/
+
+#include <stdio.h>
+
+// Helper func to find chars in a string
+int c_in_s(char c, char s[]) {
+  for (int i = 0; s[i] != '\0'; i++) {
+    if (s[i] == c)
+      return 1;
+  }
+
+  return 0;
+}
