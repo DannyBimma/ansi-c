9e2cadef2bbefe525f9813b1ef681d679d07261d
DannyBimma <dantrotman92@gmail.com>
2025-08-20 16:19:25 -0400

    Write any() function.

--- exercises/operations/any.c
+++ exercises/operations/any.c
@@ -6,3 +6,20 @@
  * (The standard library function strpbrk does the same
  * job but returns a pointer to the location.)
  */
+
+#include <stdio.h>
+
+int any(const char s1[], const char s2[]);
+
+int main(void) {}
+
+int any(const char s1[], const char s2[]) {
+  int i, j;
+
+  for (i = 0; s1[i] != '\0'; ++i)
+    for (j = 0; s2[j] != '\0'; ++j)
+      if (s1[i] == s2[j])
+        return i;
+
+  return -1;
+}
