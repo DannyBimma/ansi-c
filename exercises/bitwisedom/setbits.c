5073661560ef0d6e316a4f812d17c0088bc3d2c3
DannyBimma <dantrotman92@gmail.com>
2025-08-25 11:59:54 -0400

    Write func.

--- exercises/bitwisedom/setbits.c
+++ exercises/bitwisedom/setbits.c
@@ -3,3 +3,12 @@
  * returns x with the n bits that begin at position p set
  * to the rightmost n bits of y, leaving the other bits unchanged.
  */
+
+#include <stdio.h>
+
+unsigned setbits(unsigned x, int p, int n, unsigned y) {
+  unsigned mask = ~(~0 << n) << (p + 1 - n);
+  unsigned y_bits = (y & ~(~0 << n)) << (p + 1 - n);
+
+  return (x & ~mask) | y_bits;
+}
