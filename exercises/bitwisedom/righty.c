aeffcbbb71a957496f64e70a77364e5d61224bc5
DannyBimma <dantrotman92@gmail.com>
2025-08-25 12:10:23 -0400

    Init exercise 2-8.

---
+++ exercises/bitwisedom/righty.c
@@ -0,0 +1,7 @@
+/*Copyright (c) 2025 DannyBimma. All Rights Reserved.
+ * Exercise 2-8: Write a function rightrot(x,n) that
+ * returns the value of the integer x rotated to the
+ * right by n bit positions.
+ */
+
+#include <stdio.h>
