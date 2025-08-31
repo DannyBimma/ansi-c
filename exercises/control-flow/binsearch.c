62d5c14fe97bcfbf01e33dc337e8ce0ff916c493
DannyBimma <dantrotman92@gmail.com>
2025-08-30 21:57:34 -0400

    Write bin search func.

--- exercises/control-flow/binsearch.c
+++ exercises/control-flow/binsearch.c
@@ -6,4 +6,16 @@ int binsearch(int x, int v[], int n) {
   int low, high, mid;
   low = 0;
   high = mid - 1;
+
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (x < v[mid])
+      high = mid - 1;
+    else if (x > v[mid])
+      low = mid + 1;
+    else
+      return mid;
+  }
+
+  return -1;
 }
