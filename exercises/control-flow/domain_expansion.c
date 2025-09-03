d42ce0f4bdffc0344488ae77ba86d4062d884a4f
DannyBimma <dantrotman92@gmail.com>
2025-09-03 18:46:35 -0400

    Write expand function.

--- exercises/control-flow/domain_expansion.c
+++ exercises/control-flow/domain_expansion.c
@@ -16,4 +16,36 @@ void expand(char s1[], char s2[]) {
   int i = 0; // for input
   int j = 0; // for output
   int c;
+
+  // Process input string
+  while ((c = s1[i]) != '\0') {
+    // Range pattern check
+    if (s1[i + 1] == '-' && s1[i + 2] != '\0') {
+      // Pattern validation: must be alphanumeric and same type
+      if ((isalnum(c) && isalnum(s1[i + 2])) &&
+          ((islower(c) && islower(s1[i + 2])) ||
+           (isupper(c) && isupper(s1[i + 2])) ||
+           (isdigit(c) && isdigit(s1[i + 2])))) {
+
+        // Only expand if range is ascending
+        if (c <= s1[i + 2]) {
+          while (c <= s1[i + 2])
+            s2[j++] = c++;
+          // Skip range pattern
+          i += 3;
+        } else {
+          // Invalid order
+          s2[j++] = s1[i++];
+        }
+      } else {
+        // Invalid pattern
+        s2[j++] = s1[i++];
+      }
+    } else {
+      // Ain't no pattern found
+      s2[j++] = s1[i++];
+    }
+  }
+  // end scene
+  s2[j] = '\0';
 }
