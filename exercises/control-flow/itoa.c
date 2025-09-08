54633ac9d31feb78d831a55108b32d33e75dd4f4
DannyBimma <dantrotman92@gmail.com>
2025-09-07 21:50:26 -0400

    Write improved itoa.

--- exercises/control-flow/itoa.c
+++ exercises/control-flow/itoa.c
@@ -33,3 +33,21 @@ void reverse(char s[]) {
     s[j] = c;
   }
 }
+
+void itoa(int n, char s[]) {
+  int i, sign;
+
+  sign = n;
+
+  i = 0;
+  do {
+    s[i++] = abs(n % 10) + '0';
+  } while (n /= 10);
+
+  if (sign < 0)
+    s[i++] = '-';
+
+  s[i] = '\0';
+
+  reverse(s);
+}
