68b8d9f677a5694cc09c3aa42eda288ec0c74a63
DannyBimma <dantrotman92@gmail.com>
2025-09-07 21:47:06 -0400

    Write reverse.

--- exercises/control-flow/itoa.c
+++ exercises/control-flow/itoa.c
@@ -22,3 +22,14 @@
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
+
+void reverse(char s[]) {
+  int i, j;
+  char c;
+
+  for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
