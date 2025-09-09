1614553709e9fc0265d72ef65d734f9751b4c83f
DannyBimma <dantrotman92@gmail.com>
2025-09-08 20:18:55 -0400

    Init exercise.

---
+++ exercises/control-flow/itob.c
@@ -0,0 +1,8 @@
+/*Copyright (c) 2025 DannyBimma. All Rights Reserved.
+ * Exercise 3-5. Write the function itob(n,s,b) that converts
+ * the integer n into a base b character representation in the
+ * string s. In particular, itob(n,s,16) formats n as a hexadecimal
+ * integer in s.*/
+
+#include <stdio.h>
+#include <string.h>
