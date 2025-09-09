fa4ea5b5fa6411abb06fad81067520e11ad52c8d
DannyBimma <dantrotman92@gmail.com>
2025-09-08 20:55:50 -0400

    Init exercise.

---
+++ exercises/control-flow/itoa2.c
@@ -0,0 +1,8 @@
+/*Copyright (c) 2025 DannyBimma. All Rights Reserved.
+ * Write a version of itoa that accepts three arguments
+ * instead of two. The third argument is a minimum
+ * field width; the converted number must be padded with
+ * blanks on the left if necessary to make it wide enough.*/
+
+#include <stdio.h>
+#include <string.h>
