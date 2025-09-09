f3c9d40a5838789dc902824bc44aa230cb882fec
DannyBimma <dantrotman92@gmail.com>
2025-09-08 21:00:38 -0400

    Redefine itoa.

--- exercises/control-flow/itoa2.c
+++ exercises/control-flow/itoa2.c
@@ -6,3 +6,31 @@
 
 #include <stdio.h>
 #include <string.h>
+
+void itoa(int n, char s[], int width) {
+  int i, sign, len, padding;
+
+  if ((sign = n) < 0)
+    n = -n;
+
+  i = 0;
+  do {
+    s[i++] = n % 10 + '0';
+  } while ((n /= 10) > 0);
+
+  if (sign < 0)
+    s[i++] = '-';
+
+  len = i;
+
+  if (len < width) {
+    padding = width - len;
+
+    for (int j = 0; j < padding; j++)
+      s[i++] = ' ';
+  }
+
+  s[i] = '\0';
+
+  reverse(s);
+}
