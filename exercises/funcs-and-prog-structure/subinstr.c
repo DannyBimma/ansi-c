6d72f479f3f6621a95111aeca9bdebfffe85c260
DannyBimma <dantrotman92@gmail.com>
2025-09-10 20:58:18 -0400

    Init vars.

--- exercises/funcs-and-prog-structure/subinstr.c
+++ exercises/funcs-and-prog-structure/subinstr.c
@@ -2,3 +2,13 @@
  * Write the function strrindex(s,t), which returns the
  * position of the rightmost occurrence of t in s, or
  * −1 if there is none.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int strrin(char s[], char t[]) {
+  int i, j, k;
+  int final_pos = -1; // Nothing found
+  int slen = strlen(s);
+  int tlen = strlen(t);
+}
