140924ce25dce5beb0f9046717319a4626ef211a
DannyBimma <dantrotman92@gmail.com>
2025-09-10 20:49:20 -0400

    Init exercise.

---
+++ exercises/funcs-and-prog-structure/subinstr.c
@@ -0,0 +1,4 @@
+/*Copyright (c) 2025 DannyBimma. All Rights Reserved.
+ * Write the function strrindex(s,t), which returns the
+ * position of the rightmost occurrence of t in s, or
+ * −1 if there is none.*/
