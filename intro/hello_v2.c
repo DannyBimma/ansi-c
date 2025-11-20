b53ba8bf415c91cb70de6ad19a5826f12e06576c
DannyBimma <dantrotman92@gmail.com>
2025-11-20 19:26:51 -0400

    Complicate the greeting.

---
+++ intro/hello_v2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("H");
+  printf("e");
+  printf("l");
+  printf("l");
+  printf("o");
+  printf("\x2C");
+  printf("\x20");
+  printf("w");
+  printf("o");
+  printf("r");
+  printf("l");
+  printf("d");
+  printf("\x21");
+  printf("\n");
+}
