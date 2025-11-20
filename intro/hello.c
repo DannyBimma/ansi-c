ba37c96375625eb16b5f7684f86d4e2b188401c2
DannyBimma <dantrotman92@gmail.com>
2025-11-19 20:45:34 -0400

    Update .gitignore | Add copyright info.

--- intro/hello.c
+++ intro/hello.c
@@ -1,3 +1,14 @@
+/*
+ * Routine: A highly complex computer program, designed to run on all major
+ * operating systems, that outputs the legendary phrase "Hello, world." to the
+ * terminal with a processed new line.
+ *
+ * Author: DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ *
+ */
+
 #include <stdio.h>
 
 int main(void) {
