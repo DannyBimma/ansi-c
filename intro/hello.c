a41b62ccd7601b8b7043e3c8c48f43d62bea138f
DannyBimma <dantrotman92@gmail.com>
2025-11-19 21:04:07 -0400

    Annotate the code.

--- intro/hello.c
+++ intro/hello.c
@@ -9,10 +9,16 @@
  *
  */
 
+// Link the entire C standard library, just so you can use one single function
+// defined in it
 #include <stdio.h>
 
+// Define the function that will be the main entry point to this program
 int main(void) {
+  // call from main: a standard library function that will output the console,
+  // the sequence of chars passed to it as an argument
   printf("Hello, world!\n");
 
+  // Return exit code 0 from main to indicate great success 👍🏾
   return 0;
 }
