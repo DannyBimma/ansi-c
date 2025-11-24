a1f497d454ebcfe25ee8687f976c81da3e41b7f4
DannyBimma <dantrotman92@gmail.com>
2025-11-23 20:15:41 -0400

    Init ex 1-3.

---
+++ exercises/ex1-3.c
@@ -0,0 +1,8 @@
+/*
+ * Routine: A Fahrenheit to Celsius temperature conversion table: from 0 to 420
+ *
+ * Author: DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ *
+ */
