adcb5ec3940b4fe6b3a51f4ba6f32b681b57029f
DannyBimma <dantrotman92@gmail.com>
2025-11-23 20:26:52 -0400

    Write tmep program.

--- exercises/ex1-3.c
+++ exercises/ex1-3.c
@@ -6,3 +6,22 @@
  * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
  *
  */
+
+#include <stdio.h>
+
+int main(void) {
+  float fahrenheit, celsius;
+  int lower_temp, upper_temp, increment;
+
+  lower_temp = 0;
+  upper_temp = 0;
+  increment = 20;
+
+  fahrenheit = lower_temp;
+
+  while (fahrenheit <= upper_temp) {
+    celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
+
+    printf("%3.0f %6.1f\n", fahrenheit, celsius);
+  }
+}
