b33da7e91ddbcfc194009aedb31ad783c2e77870
DannyBimma <dantrotman92@gmail.com>
2025-11-23 20:39:34 -0400

    Fix infinite loop | Complete ex 1-3 | Add temp symbols.

--- exercises/ex1-3.c
+++ exercises/ex1-3.c
@@ -14,14 +14,20 @@ int main(void) {
   int lower_temp, upper_temp, increment;
 
   lower_temp = 0;
-  upper_temp = 0;
+  upper_temp = 420;
   increment = 20;
 
   fahrenheit = lower_temp;
 
+  printf("\n");
+  printf("++=== THE ULTIMATE TEMPERATURE CONVERSION TABLE ===++");
+  printf("\n");
+
   while (fahrenheit <= upper_temp) {
     celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
 
-    printf("%3.0f %6.1f\n", fahrenheit, celsius);
+    printf("%3.0f °F %6.1f °C\n", fahrenheit, celsius);
+
+    fahrenheit = fahrenheit + increment;
   }
 }
