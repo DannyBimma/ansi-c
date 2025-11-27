fa252bafa7ace7a8f2dcc63a232619fe3c393d96
DannyBimma <dantrotman92@gmail.com>
2025-11-26 22:05:14 -0400

    Init exercise.

---
+++ exercises/ex-1-5.c
@@ -0,0 +1,9 @@
+/*
+ * Routine: Modify the temperature conversion program to print the table in
+ * reverse order, that is, from 300 degrees to 0.
+ *
+ * Author: DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ *
+ */
