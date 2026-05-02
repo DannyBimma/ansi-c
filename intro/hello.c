f149771daee039aa70a741a0197f996aa8e8ed7b
DannyBimma <dantrotman92@gmail.com>
2026-05-02 13:15:00 -0400

    doc edits.

--- intro/hello.c
+++ intro/hello.c
@@ -1,22 +1,21 @@
 /*
  * Routine: A highly complex computer program, designed to run on all major
- * operating systems, that outputs the legendary phrase "Hello, world." to the
- * terminal with a processed new line.
+ *          operating systems, that outputs the legendary phrase "Hello, world."
+ *          to the terminal with a processed new line.
  *
- * Author: DannyBimma
+ * Author:  DannyBimma
  *
  * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
  *
  */
 
-// Link the entire C standard library, just so you can use one single function
-// defined in it
+// Link the entire C standard I/O Library, in order to use one single function
+// defined within
 #include <stdio.h>
 
-// Define the function that will be the main entry point to this program
+// main function, the classic entry point to every C program
 int main(void) {
-  // call from main: a standard library function that will output the console,
-  // the sequence of chars passed to it as an argument
+  // Output the legendary phrase to the terminal
   printf("Hello, world!\n");
 
   // Return exit code 0 from main to indicate great success 👍🏾
