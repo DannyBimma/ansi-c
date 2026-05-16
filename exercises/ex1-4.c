b66b367459b648a93c320c5ae63081b723bfb2b5
DannyBimma <dantrotman92@gmail.com>
2026-05-16 12:36:04 -0400

    formatting.

--- exercises/ex1-4.c
+++ exercises/ex1-4.c
@@ -10,24 +10,24 @@
 #include <stdio.h>
 
 int main(void) {
-  float fahrenheit, celsius;
-  int lower_temp, upper_temp, increment;
+    float fahrenheit, celsius;
+    int lower_temp, upper_temp, increment;
 
-  lower_temp = 0;
-  upper_temp = 420;
-  increment = 10;
+    lower_temp = 0;
+    upper_temp = 420;
+    increment = 10;
 
-  celsius = lower_temp;
+    celsius = lower_temp;
 
-  printf("\n");
-  printf("++=== THE ULTIMATE TEMPERATURE CONVERSION TABLE ===++");
-  printf("\n");
+    printf("\n");
+    printf("++=== THE ULTIMATE TEMPERATURE CONVERSION TABLE ===++");
+    printf("\n");
 
-  while (celsius <= upper_temp) {
-    fahrenheit = (9.0 / 5.0) * celsius + 32.0;
+    while (celsius <= upper_temp) {
+        fahrenheit = (9.0 / 5.0) * celsius + 32.0;
 
-    printf("%3.0f °C %6.1f °F\n", celsius, fahrenheit);
+        printf("%3.0f °C %6.1f °F\n", celsius, fahrenheit);
 
-    celsius = celsius + increment;
-  }
+        celsius = celsius + increment;
+    }
 }
