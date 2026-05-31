d63c59cc95504e29aafb3ea4bc122235b7a817bb
DannyBimma <dantrotman92@gmail.com>
2026-05-30 21:06:56 -0400

    complete exercise 1-9.

---
+++ exercises/ex1-9.c
@@ -0,0 +1,26 @@
+/*
+ * Routine: A program that copies its input to its output, replacing
+ *          each string of one or more blanks by a single blank.
+ *
+ * Author:  DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ *
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int ch;
+    int prev_ch = EOF;
+
+    while ((ch = getchar()) != EOF) {
+        // If the current char OR previous char is not a blank, then print it
+        if (ch != ' ' || prev_ch != ' ')
+            putchar(ch);
+
+        prev_ch = ch;
+    }
+
+    return 0;
+}
