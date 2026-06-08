c3973f8f647cad62e82169f1c0e882a6b58eb68d
DannyBimma <dantrotman92@gmail.com>
2026-06-08 12:48:28 -0400

    write the example array program.

---
+++ basics/cc.c
@@ -0,0 +1,39 @@
+/*
+ A program that counts the number of occurrences of each digit,
+ of white space characters (blanks, tabs, newline), and of all
+ other characters. Theses counts will be stored in an array becasue
+ K&R said it's more convenient that way innit!?
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int ch;
+    int i;
+    int nwhite = 0;
+    int nother = 0;
+    int ndigit[10];
+
+    for (int i = 0; i < 10; ++i)
+        ndigit[i] = 0;
+
+    while ((ch = getchar()) != EOF) {
+        // check if the current char is a digit
+        if (ch >= '0' && ch <= '9')
+            // convert the char code back into it's numerical value, and
+            // increment count
+            ++ndigit[ch - '0'];
+        else if (ch == ' ' || ch == '\n' || ch == '\t')
+            ++nwhite;
+        else
+            ++nother;
+    }
+
+    printf("digits=");
+    for (i = 0; i < 10; ++i)
+        printf(" %d", ndigit[i]);
+
+    printf(", white space = %d, other = %d\n", nwhite, nother);
+
+    return 0;
+}
