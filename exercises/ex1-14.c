dbc504d10899c62699e3246aab69c8253eede814
DannyBimma <dantrotman92@gmail.com>
2026-06-09 15:51:25 -0400

    init exercise 1-14.

---
+++ exercises/ex1-14.c
@@ -0,0 +1,9 @@
+/*
+ * Routine: A program that prints a histogram of the frequencies of
+ *          different characters in its input.
+ *
+ * Author:  DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ *
+ */
