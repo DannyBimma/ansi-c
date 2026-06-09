396bb676d98838d4628b89f8cfd8dd07cc90a934
DannyBimma <dantrotman92@gmail.com>
2026-06-09 16:16:19 -0400

    compile and test.

--- exercises/ex1-14.c
+++ exercises/ex1-14.c
@@ -17,13 +17,29 @@ int main(void) {
     int cc[ALL_CHARS];
 
     // Zero out char count array
-    for (int i = 0; i < ALL_CHARS; ++i) {
+    for (int i = 0; i < ALL_CHARS; ++i)
         cc[i] = 0;
-    }
 
     // Read from input and increment char ASCII slot in array
     while ((ch = getchar()) != EOF) {
         if (ch >= 0 && ch < ALL_CHARS)
             ++cc[ch];
     }
+
+    // Print histogram of printable chars from input
+    printf("\n--- Histogram of Printable ASCII Characters ---\n");
+    for (int i = 32; i < 126; ++i) {
+        // Only print bars for chars that appeared
+        if (cc[i] > 0) {
+            // Print label for each bar of stars in histogram
+            printf("%c: ", i);
+            // Print a bar of stars (how many times this char appeared)
+            for (int j = 0; j < cc[i]; ++j)
+                putchar('*');
+
+            putchar('\n');
+        }
+    }
+
+    return 0;
 }
