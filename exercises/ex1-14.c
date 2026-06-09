759610a8a613768238db0689e098d4aaf74f70b1
DannyBimma <dantrotman92@gmail.com>
2026-06-09 16:03:19 -0400

    read chars from input.

--- exercises/ex1-14.c
+++ exercises/ex1-14.c
@@ -7,3 +7,23 @@
  * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
  *
  */
+
+#include <stdio.h>
+
+#define ALL_CHARS 256 // Total num of chars in the ASCII table
+
+int main(void) {
+    int ch;
+    int cc[ALL_CHARS];
+
+    // Zero out char count array
+    for (int i = 0; i < ALL_CHARS; ++i) {
+        cc[i] = 0;
+    }
+
+    // Read from input and increment char ASCII slot in array
+    while ((ch = getchar()) != EOF) {
+        if (ch >= 0 && ch < ALL_CHARS)
+            ++cc[ch];
+    }
+}
