e57213538c839581b8d7ae4f4389e8fe2da45cc1
DannyBimma <dantrotman92@gmail.com>
2026-06-10 17:25:29 -0400

    init max line example program.

--- basics/power.c
+++ basics/power.c
@@ -7,9 +7,7 @@ int power(int base, int exp);
 
 int main(void) {
     for (int i = 0; i < 10; ++i)
-        printf("%d %d %d", i, power(2, i), power(-3, i));
-
-    putchar('\n');
+        printf("%d %d %d\n", i, power(2, i), power(-3, i));
 
     return 0;
 }
