5d0d805aefaed766e599b70ff62d597141bf2d3f
DannyBimma <dantrotman92@gmail.com>
2026-06-10 13:16:15 -0400

    compile and test.

--- exercises/ex1-15.c
+++ exercises/ex1-15.c
@@ -11,12 +11,30 @@
 #include <stdio.h>
 
 // Prototype
-float converter(float fah, float cel);
+float converter(float cel);
 
-int main(void) {}
+int main(void) {
+    float fahrenheit, celsius;
+    int lower_temp, upper_temp, increment;
 
-float converter(float fah, float cel) {
-    fah = (9.0 / 5.0) * cel + 32.0;
+    lower_temp = 0;
+    upper_temp = 420;
+    increment = 10;
 
-    return fah;
+    celsius = lower_temp;
+
+    printf("\n");
+    printf("++=== THE ULTIMATE TEMPERATURE CONVERSION TABLE V2 ===++");
+    printf("\n");
+
+    while (celsius <= upper_temp) {
+        fahrenheit = converter(celsius);
+        printf("%3.0f C  %6.1f F\n", celsius, fahrenheit);
+
+        celsius = celsius + increment;
+    }
+
+    return 0;
 }
+
+float converter(float cel) { return (9.0 / 5.0) * cel + 32.0; }
