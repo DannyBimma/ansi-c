31607b5ff287a706b1eaf1935ef3710a55735ea0
DannyBimma <dantrotman92@gmail.com>
2026-06-09 21:05:53 -0400

    write power function example.

---
+++ basics/power.c
@@ -0,0 +1,22 @@
+/*A that raises an integer to the power of another integer*/
+
+#include <stdio.h>
+
+// Prototype
+int power(int base, int exp);
+
+int main(void) {
+    for (int i = 0; i < 10; ++i)
+        printf("%d %d %d", i, power(2, i), power(-3, i));
+
+    return 0;
+}
+
+int power(int base, int exp) {
+    int res = 1;
+
+    for (int i = 1; i <= exp; ++i)
+        res *= base;
+
+    return res;
+}
