304476868e7aaab4454a82fdaf02374f701e86c1
DannyBimma <dantrotman92@gmail.com>
2026-06-10 21:49:22 -0400

    write main logic.

--- exercises/ex1-17.c
+++ exercises/ex1-17.c
@@ -1,10 +1,27 @@
 /*
- * Routine: A revision of the "longest line" program to correctly print the
- *          length of arbitrarily long input lines, and as much as possible
- *          of the text.
+ * Routine: A program that prints all input lines that are longer than 80
+ *          characters.
  *
  * Author: DannyBimma
  *
  * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
  *
  */
+
+#include <stdio.h>
+
+#define MAX 1000 // buffer size
+#define LONG 80  // minimum length to print
+
+int get_line(char s[], int lim);
+
+int main(void) {
+    int len;
+    char line[MAX];
+
+    while ((len = get_line(line, MAX)) > 0)
+        if (len > LONG)
+            printf("%s", line);
+
+    return 0;
+}
