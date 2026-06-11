c2e1eb8b01f0ac49bb50e9a9861a8b32e7f590a5
DannyBimma <dantrotman92@gmail.com>
2026-06-10 21:36:15 -0400

    init exercise 1-17.

---
+++ exercises/ex1-17.c
@@ -0,0 +1,10 @@
+/*
+ * Routine: A revision of the "longest line" program to correctly print the
+ *          length of arbitrarily long input lines, and as much as possible
+ *          of the text.
+ *
+ * Author: DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ *
+ */
